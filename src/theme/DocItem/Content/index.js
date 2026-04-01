f13b0850486368f5fdf9a5a82f15dea8255983b5
vaishnavs0 <vaishnav.chandurkar@quilr.ai>
2026-04-01 11:19:16 +0530

    feat: add copy button and ask ai dropdown

---
+++ src/theme/DocItem/Content/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
+import Heading from "@theme/Heading";
+import MDXContent from "@theme/MDXContent";
+import DocPageCopyDropdown from "@site/src/components/DocPageCopyDropdown";
+
+function useSyntheticTitle() {
+  const { metadata, frontMatter, contentTitle } = useDoc();
+  const shouldRender =
+    !frontMatter.hide_title && typeof contentTitle === "undefined";
+  if (!shouldRender) {
+    return null;
+  }
+  return metadata.title;
+}
+
+export default function DocItemContent({ children }) {
+  const syntheticTitle = useSyntheticTitle();
+
+  return (
+    <div className={clsx(ThemeClassNames.docs.docMarkdown, "markdown")}>
+      <div className="relative">
+        <div className="absolute top-0 right-0">
+          <DocPageCopyDropdown />
+        </div>
+        <MDXContent>{children}</MDXContent>
+      </div>
+    </div>
+  );
+}
